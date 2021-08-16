109bef2783fbea5eafa21320b885c7fbf65f070e
Alex-Buttielie <63355403+Alex-Buttielie@users.noreply.github.com>
2021-08-16 19:46:55 -0300

    [004 - Mudança da versão do express validator]

--- cursoNode/app/models/NoticiasDAO.js
+++ cursoNode/app/models/NoticiasDAO.js
@@ -1,24 +1,28 @@
-function NoticiasDAO(connection){
+function NoticiasDAO(connection) {
     this._connection = connection;
 }
 
-NoticiasDAO.prototype.getNoticias = function(callback){
+NoticiasDAO.prototype.getNoticias = function (callback) {
     var query = 'select * from noticias';
     this._connection.query(query, callback);
 
 }
 
-NoticiasDAO.prototype.getNoticia = function(callback){
+NoticiasDAO.prototype.getNoticia = function (callback) {
     var query = 'select * from noticias where id_noticias = 24';
     this._connection.query(query, callback);
 
 }
 
-NoticiasDAO.prototype.salvarNoticias = function(noticia, callback){
-    var query = "insert into noticias (titulo, noticia) values ('" +   noticia.titulo + "','" + noticia.noticia + "')";
+NoticiasDAO.prototype.salvarNoticias = function (noticia, callback) {
+    var query = "insert into noticias (titulo, noticia, resumo, nome_autor, data_noticia) values ('"
+        + noticia.titulo + "','" + noticia.noticia + "','"
+        + noticia.resumo + "','" + noticia.nome_autor + "','" + noticia.data_noticia + "')";
+
+
     this._connection.query(query, callback);
 }
 
 module.exports = function () {
-        return NoticiasDAO;
+    return NoticiasDAO;
 }

07db8ff0a633cf302e4c142f2c4cd0f7991a74f4
Alex-Buttielie <63355403+Alex-Buttielie@users.noreply.github.com>
2021-08-17 19:09:53 -0300

    [008 - colcando a pagina de notícias dinamicas]

--- cursoNode/app/controllers/home.js
+++ cursoNode/app/controllers/home.js
@@ -2,8 +2,8 @@ module.exports.index = function (application, req, res) {
         var connection = application.config.dbConnection();
         var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
-        noticiasModel.getCincoUltimasNoticias(function(error, result){
+        noticiasModel.getCincoUltimasNoticias(function(_error, result){
                 console.log(result);
-                res.render('home/index', {noticias : result});
+                res.render("home/index", {noticias : result.rows});
         }); 
 }
